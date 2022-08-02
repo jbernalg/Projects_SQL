bd1a7de7a87ef5b409f967041a997f8c70b4bd13
jbernalg <jeinffersonbernal@gmail.com>
2022-08-02 15:45:35 -0500

    Consulta SQL 24

--- Consultas_Gestion_Empleados.sql
+++ Consultas_Gestion_Empleados.sql
@@ -299,6 +299,17 @@ SELECT nombre, presupuesto
 FROM departamento
 WHERE presupuesto < 100000 OR presupuesto > 200000;
 
+-- Devuelve una lista con el nombre de los departamentos que tienen un presupuesto entre 100000 y 200000 euros. Utilizando el operador BETWEEN.
+SELECT nombre, presupuesto
+FROM departamento
+WHERE presupuesto BETWEEN 100000 AND 200000;
+
+-- Devuelve una lista con el nombre de los departamentos que no tienen un presupuesto entre 100000 y 200000 euros. 
+-- Utilizando el operador BETWEEN.
+SELECT nombre, presupuesto
+FROM departamento
+WHERE presupuesto NOT BETWEEN 100000 AND 200000;
+
 
 
 
