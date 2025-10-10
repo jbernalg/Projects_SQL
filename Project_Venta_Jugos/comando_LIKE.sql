62dd4e146935fa6acc56a114a09ba2161560a6d1
jbernalg <jeinffersonbernal@gmail.com>
2025-10-10 07:38:30 -0500

    comando LIKE

---
+++ Project_Venta_Jugos/comando_LIKE.sql
@@ -0,0 +1,14 @@
+USE jugos_ventas;
+
+SELECT * 
+FROM tabla_de_productos
+WHERE SABOR LIKE '%manzana'; 
+
+SELECT * 
+FROM tabla_de_productos
+WHERE SABOR LIKE '%manzana'
+	AND ENVASE = 'Botella PET';
+    
+SELECT *
+FROM tabla_de_clientes
+WHERE NOMBRE LIKE '%ez';
