dae3db364cd73f06d0f7dcb4143d3989c30983c7
jbernalg <jeinffersonbernal@gmail.com>
2025-10-11 07:43:41 -0500

    comando order by

---
+++ Project_Venta_Jugos/comando_ORDER_BY.sql
@@ -0,0 +1,18 @@
+SELECT *
+FROM tabla_de_productos;
+
+SELECT *
+FROM tabla_de_productos
+ORDER BY PRECIO_DE_LISTA;
+
+SELECT *
+FROM tabla_de_productos
+ORDER BY PRECIO_DE_LISTA DESC;
+
+SELECT *
+FROM tabla_de_productos
+ORDER BY NOMBRE_DEL_PRODUCTO;
+
+SELECT *
+FROM tabla_de_productos
+ORDER BY ENVASE DESC, NOMBRE_DEL_PRODUCTO ASC;
