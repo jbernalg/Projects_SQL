6ee9babcb6a688671ddb9dba40df242dd483559e
jbernalg <jeinffersonbernal@gmail.com>
2025-10-21 10:37:29 -0500

    funciones strings

---
+++ Project_Venta_Jugos/funciones_STRINGS.sql
@@ -0,0 +1,11 @@
+SELECT LTRIM("   MySQL es facil");
+
+SELECT LTRIM("MySQL es facil    ");
+
+SELECT TRIM("    MySQL es facil    ");
+
+SELECT CONCAT("MySQL es facil", " No lo crees?");
+
+SELECT UPPER("mysql es una base de datos interesante"); 
+
+SELECT LOWER("MYSQL ES UNA BASE DE DATOS INTERESANTE");
