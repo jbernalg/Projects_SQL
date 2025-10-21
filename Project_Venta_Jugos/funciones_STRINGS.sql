b08e55c3d295f10209e85cab7793ca583c632e45
jbernalg <jeinffersonbernal@gmail.com>
2025-10-21 11:15:43 -0500

    funciones strings 1.2

--- Project_Venta_Jugos/funciones_STRINGS.sql
+++ Project_Venta_Jugos/funciones_STRINGS.sql
@@ -1,3 +1,5 @@
+USE jugos_ventas;
+
 SELECT LTRIM("   MySQL es facil");
 
 SELECT LTRIM("MySQL es facil    ");
@@ -9,3 +11,13 @@ SELECT CONCAT("MySQL es facil", " No lo crees?");
 SELECT UPPER("mysql es una base de datos interesante"); 
 
 SELECT LOWER("MYSQL ES UNA BASE DE DATOS INTERESANTE");
+
+SELECT SUBSTRING("Mysql es una base de datos interesante", 14, 4);
+
+SELECT CONCAT(NOMBRE, " ", DNI)
+FROM tabla_de_clientes;
+
+SELECT 
+	NOMBRE,
+    CONCAT(DIRECCION_1, " ", BARRIO, " ", CIUDAD, " ", ESTADO) AS COMPLETO
+FROM tabla_de_clientes;
