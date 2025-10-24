5c51c74f84205b13ce48eb796af672c826b034ee
jbernalg <jeinffersonbernal@gmail.com>
2025-10-24 07:02:19 -0500

    funciones de fecha 1.2

--- Project_Venta_Jugos/funciones_DATE.sql
+++ Project_Venta_Jugos/funciones_DATE.sql
@@ -14,4 +14,22 @@ SELECT MONTHNAME(current_timestamp());
 
 SELECT DAYNAME(current_timestamp());
 
-SELECT DATEDIFF(current_timestamp(), '2025-01-01');
+SELECT DATEDIFF(current_timestamp(), '2025-01-01') AS DIFERENCIA_DIAS;
+
+SELECT current_timestamp() AS DIA_HOY,
+DATE_SUB(current_timestamp(), INTERVAL 1 MONTH) AS RESULTADO;
+
+SELECT DISTINCT 
+	FECHA_VENTA,
+	DAYNAME(FECHA_VENTA) AS DIA,
+    MONTHNAME(FECHA_VENTA) AS MES,
+    YEAR(FECHA_VENTA) AS ANO
+FROM facturas;
+
+SELECT
+	NOMBRE,
+    timestampdiff(YEAR, FECHA_DE_NACIMIENTO, CURDATE()) AS EDAD
+FROM tabla_de_clientes;
+
+
+
