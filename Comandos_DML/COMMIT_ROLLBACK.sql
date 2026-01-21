482227a2e6fd9c80e80d4290fe4e95940f090ed5
jbernalg <jeinffersonbernal@gmail.com>
2026-01-21 07:25:04 -0500

    ejemplo completo commit start transaction

--- Comandos_DML/COMMIT_ROLLBACK.sql
+++ Comandos_DML/COMMIT_ROLLBACK.sql
@@ -19,3 +19,46 @@ VALUES
 
 -- mostrar datos
 SELECT * FROM tb_vendedor;
+
+-- iniciar transaccion
+START TRANSACTION;
+
+-- insertamos nuevos registros en la BD
+INSERT INTO `ventas_jugos`.`tb_vendedor`
+(`MATRICULA`,
+`NOMBRE`,
+`BARRIO`,
+`COMISION`,
+`FECHA_ADMISION`,
+`DE_VACACIONES`)
+VALUES
+('257',
+'Fernando Rojas',
+'Oblatos',
+0.1,
+'2015-06-14',
+0);
+
+INSERT INTO `ventas_jugos`.`tb_vendedor`
+(`MATRICULA`,
+`NOMBRE`,
+`BARRIO`,
+`COMISION`,
+`FECHA_ADMISION`,
+`DE_VACACIONES`)
+VALUES
+('258',
+'David Rojas',
+'Del Valle',
+0.15,
+'2015-06-14',
+0);
+
+-- actualizar datos de registros
+UPDATE tb_vendedor SET COMISION = COMISION * 1.05;
+
+-- rollback: nos lleva a la BD antes de agregar los datos
+ROLLBACK;
+
+-- commit: confirma la transaccion y sube los cambios a la BD
+COMMIT;
