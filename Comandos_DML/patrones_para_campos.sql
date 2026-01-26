1c4ee1d73b1fc6480145550c9a93e45f0bc73449
jbernalg <jeinffersonbernal@gmail.com>
2026-01-26 13:58:25 -0500

    ejercicio patrones para campos

--- Comandos_DML/patrones_para_campos.sql
+++ Comandos_DML/patrones_para_campos.sql
@@ -9,3 +9,16 @@ CREATE TABLE tb_default(
     FECHA_CREACION TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
     PRIMARY KEY(ID)
 );
+
+-- insertar registro a la tabla
+INSERT INTO tb_default
+(DESCRIPCION, DIRECCION, CIUDAD, FECHA_CREACION)
+VALUES ('Cliente X', 'Calle Sol 525', 'Cancun', '2026-01-05');
+
+-- mostrar valores de la tabla
+SELECT * FROM tb_default;
+
+-- insertar registros con informacion en algunos campos
+INSERT INTO tb_default
+(DESCRIPCION)
+VALUES ('Cliente Y');
