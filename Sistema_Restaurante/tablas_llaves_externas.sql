79d05ea53322b1addf4cd1ff1d70cd7e45252042
jbernalg <jeinffersonbernal@gmail.com>
2026-02-17 09:00:17 -0500

    tablas pedidos

--- Sistema_Restaurante/tablas_llaves_externas.sql
+++ Sistema_Restaurante/tablas_llaves_externas.sql
@@ -6,3 +6,15 @@ ALTER TABLE productos
 ADD CONSTRAINT fk_categoria
 FOREIGN KEY (id_categoria)
 REFERENCES categorias(id_categoria);
+
+CREATE TABLE pedidos (
+	id_pedido INT PRIMARY KEY AUTO_INCREMENT,
+    id_cliente INT,
+    id_empleado INT,
+    fecha_pedido TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    estado VARCHAR(30) DEFAULT 'pendiente',
+    total DECIMAL(10,2),
+    activo TINYINT NOT NULL DEFAULT 1,
+    FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente),
+    FOREIGN KEY (id_empleado) REFERENCES empleados(id_empleado)
+);
