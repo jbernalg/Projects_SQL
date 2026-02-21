79a9d4b4c6490b689e7bd1e9f38b9db9c4fb97d9
jbernalg <jeinffersonbernal@gmail.com>
2026-02-21 09:15:59 -0500

    arreglo unicidad del campo empleados

--- Sistema_Restaurante/insertar_datos.sql
+++ Sistema_Restaurante/insertar_datos.sql
@@ -30,6 +30,7 @@ INSERT INTO empleados (nombre, cargo, salario, fecha_contratacion) VALUES
 ('Pedro Lopez', 'Mesero', 1500000, '2023-02-15');
 
 select * from productos;
+select * from empleados;
 
 -- verificar la insersion de duplicados
 INSERT INTO productos (nombre, precio, id_categoria) VALUES
@@ -37,11 +38,24 @@ INSERT INTO productos (nombre, precio, id_categoria) VALUES
 
 INSERT INTO categorias (nombre) VALUES ('Bebidas');
 
+INSERT INTO clientes (nombre, email, telefono) VALUES
+('Carlos Ramirez', 'carlos@gmail.com', '3001112233');
+
+INSERT INTO empleados (nombre, cargo, salario, fecha_contratacion) VALUES
+('Laura Martinez', 'Mesero', 1500000, '2023-01-10');
+
 -- identificar productos duplicados
 select
 	nombre,
     COUNT(*)
 from productos
+group by nombre
+having count(*) > 1;
+
+select
+	nombre,
+    COUNT(*)
+from empleados
 group by nombre
 having count(*) > 1;
 
@@ -52,8 +66,31 @@ join productos p2
 ON p1.nombre = p2.nombre
 AND p1.id_producto > p2.id_producto;
 
+delete e1
+from empleados e1
+join empleados e2
+ON e1.nombre = e2.nombre
+AND e1.id_empleado > e2.id_empleado;
+
 -- agregar restriccion unioque a la tabla productos
 ALTER TABLE productos
 ADD CONSTRAINT unique_nombre_producto
 UNIQUE (nombre);
 
+-- arreglar unicidad en la tabla empleados
+ALTER TABLE empleados
+ADD cedula VARCHAR(20);
+
+update empleados
+SET cedula = '12334455'
+where id_empleado = 1;
+
+update empleados
+SET cedula = '22443366'
+where id_empleado = 2;
+
+alter table empleados modify cedula varchar(20) not null;
+
+alter table empleados
+add constraint unique_cedula_empleado
+unique (cedula);
