a03929049e716c99d65d23febaa3355dfae36328
jbernalg <jeinffersonbernal@gmail.com>
2026-03-05 10:23:48 -0500

    answer questions 2

--- Sistema_Restaurante/preguntas_negocio.sql
+++ Sistema_Restaurante/preguntas_negocio.sql
@@ -2,6 +2,8 @@ USE restaurante_db;
 
 select * from productos;
 select * from detalle_pedido;
+select * from pedidos;
+select * from empleados;
 
 -- Cual es el producto mas vendido?
 SELECT
@@ -13,3 +15,13 @@ LEFT JOIN productos pr
 GROUP BY pr.nombre
 ORDER BY total_vendido DESC
 LIMIT 1;
+
+-- Cuanto ha vendido cada empleado?
+SELECT
+	e.nombre AS empleado,
+    SUM(p.total) AS total_vendido
+FROM pedidos p 
+LEFT JOIN empleados e 
+	ON p.id_empleado = e.id_empleado
+GROUP BY e.nombre
+ORDER BY total_vendido DESC;
