2de0a1ae3d5ee1a43e5ad55239d4196a5375f2cb
jbernalg <jeinffersonbernal@gmail.com>
2026-03-06 08:12:02 -0500

    limit, order by, round

--- SQL_for_Data_Scientist/command_select.sql
+++ SQL_for_Data_Scientist/command_select.sql
@@ -1,5 +1,48 @@
 USE farmers_market;
 
-SELECT * 
-FROM product
+-- mostrar los 5 primeros registros de la tabla product
+SELECT * FROM product
 LIMIT 5;
+
+-- listar market date, vendor ID y booth number de los primeros 5 registros
+-- de la tabla vendor_booth_assignments
+SELECT
+	market_date,
+    vendor_id,
+    booth_number
+FROM vendor_booth_assignments
+ORDER BY market_date
+LIMIT 5;
+
+-- mostrar el product_id y el product_name de los 5 primeros registros de la tabla product
+-- ordenando los registros alfabeticamente por product_name
+SELECT
+	product_id,
+    product_name
+FROM product
+ORDER BY product_name
+LIMIT 5;
+
+-- listar market date, vendor ID y booth number de los primeros 5 registros
+-- de la tabla vendor_booth_assignments ordenados por market_date y vendor_id
+SELECT
+	market_date,
+    vendor_id,
+    booth_number
+FROM vendor_booth_assignments
+ORDER BY market_date, vendor_id
+LIMIT 5;
+
+-- tabla customer_purchases
+SELECT * FROM customer_purchases LIMIT 5;
+
+-- obtener el price de la tabla customer_purchases
+SELECT
+	market_date,
+    customer_id,
+    quantity,
+    cost_to_customer_per_qty,
+    ROUND(quantity * cost_to_customer_per_qty,2) AS price
+FROM customer_purchases
+LIMIT 10;
+
