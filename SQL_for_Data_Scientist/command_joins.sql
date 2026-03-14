d2f12097ef1a1546be193cc716f48bb9d670d773
jbernalg <jeinffersonbernal@gmail.com>
2026-03-14 07:39:57 -0500

    left join 1

--- SQL_for_Data_Scientist/command_joins.sql
+++ SQL_for_Data_Scientist/command_joins.sql
@@ -1,2 +1,8 @@
 USE farmers_market;
 
+-- ------------- LEFT JOIN ------------------------
+-- Obtener todos los productos con su respectiva categoria
+SELECT *
+FROM product
+LEFT JOIN product_category
+	ON product.product_category_id = product_category.product_category_id;
