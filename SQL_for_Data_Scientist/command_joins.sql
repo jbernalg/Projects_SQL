22cac268ab6c839016394345f165f03d9f9fdcbf
jbernalg <jeinffersonbernal@gmail.com>
2026-03-14 07:45:22 -0500

    left join 2

--- SQL_for_Data_Scientist/command_joins.sql
+++ SQL_for_Data_Scientist/command_joins.sql
@@ -6,3 +6,14 @@ SELECT *
 FROM product
 LEFT JOIN product_category
 	ON product.product_category_id = product_category.product_category_id;
+    
+-- obtener algunos campo de todos los registros con su respectiva categoria
+SELECT
+	pr.product_id,
+    pr.product_name,
+    pr.product_category_id AS product_prod_cat_id,
+    cat.product_category_id AS category_prod_cat_id,
+    cat.product_category_name
+FROM product pr
+LEFT JOIN product_category cat
+	ON pr.product_category_id = cat.product_category_id;
