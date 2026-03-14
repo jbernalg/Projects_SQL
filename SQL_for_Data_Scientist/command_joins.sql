7801b3d06922e1acf2cc77aa9b38eaa38f8af9cb
jbernalg <jeinffersonbernal@gmail.com>
2026-03-14 08:05:17 -0500

    left join 3

--- SQL_for_Data_Scientist/command_joins.sql
+++ SQL_for_Data_Scientist/command_joins.sql
@@ -17,6 +17,19 @@ SELECT
 FROM product pr
 LEFT JOIN product_category cat
 	ON pr.product_category_id = cat.product_category_id;
+    
+-- obtener una lista de los clientes y su compra asociada, si las hay
+SELECT *
+FROM customer AS c
+LEFT JOIN customer_purchases AS cp
+	ON c.customer_id = cp.customer_id;
+    
+-- obtener los clientes que no han registrado la primera compra
+SELECT c.*
+FROM customer AS c
+LEFT JOIN customer_purchases AS cp
+	ON c.customer_id = cp.customer_id
+WHERE cp.customer_id IS NULL;
   
   
 -- -------------- RIGHT JOIN ----------------------
@@ -29,4 +42,17 @@ SELECT
     cat.product_category_name
 FROM product pr
 RIGHT JOIN product_category cat
+	ON pr.product_category_id = cat.product_category_id;
+    
+
+-- ------------------ INNER JOIN -----------------------
+-- obtener todos los productos que esten dentro de una categoria
+SELECT
+	pr.product_id,
+    pr.product_name,
+    pr.product_category_id AS product_prod_cat_id,
+    cat.product_category_id AS category_prod_cat_id,
+    cat.product_category_name
+FROM product pr
+INNER JOIN product_category cat
 	ON pr.product_category_id = cat.product_category_id;
