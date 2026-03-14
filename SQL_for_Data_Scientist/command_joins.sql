d861404fa10ec301922202085e505d3f13939386
jbernalg <jeinffersonbernal@gmail.com>
2026-03-14 07:55:03 -0500

    right join

--- SQL_for_Data_Scientist/command_joins.sql
+++ SQL_for_Data_Scientist/command_joins.sql
@@ -17,3 +17,16 @@ SELECT
 FROM product pr
 LEFT JOIN product_category cat
 	ON pr.product_category_id = cat.product_category_id;
+  
+  
+-- -------------- RIGHT JOIN ----------------------
+-- Obtener todas las categorias con sus respectivos productos
+SELECT
+	pr.product_id,
+    pr.product_name,
+    pr.product_category_id AS product_prod_cat_id,
+    cat.product_category_id AS category_prod_cat_id,
+    cat.product_category_name
+FROM product pr
+RIGHT JOIN product_category cat
+	ON pr.product_category_id = cat.product_category_id;
