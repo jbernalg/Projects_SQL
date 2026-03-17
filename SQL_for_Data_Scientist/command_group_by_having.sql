6d9fd43e8b8f89b76ee808be58e0af619b6b56ed
jbernalg <jeinffersonbernal@gmail.com>
2026-03-17 17:28:22 -0500

    command group by

---
+++ SQL_for_Data_Scientist/command_group_by_having.sql
@@ -0,0 +1,16 @@
+USE farmers_market;
+
+-- obtener una lista de los IDs de clientes que realizaron compras en cada fecha
+SELECT
+	market_date,
+    customer_id
+FROM customer_purchases
+ORDER BY market_date, customer_id;
+
+-- obtener lista de los IDs de clientes agrupados por cada fecha en que realizaron compras
+SELECT
+	market_date,
+    customer_id
+FROM customer_purchases
+GROUP BY market_date, customer_id
+ORDER BY market_date, customer_id;
