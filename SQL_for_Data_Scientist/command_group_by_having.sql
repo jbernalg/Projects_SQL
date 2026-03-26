08bfe9aa8b8ac18465a8ea432f59d0dfb36f0f40
jbernalg <jeinffersonbernal@gmail.com>
2026-03-26 06:51:46 -0500

    average

--- SQL_for_Data_Scientist/command_group_by_having.sql
+++ SQL_for_Data_Scientist/command_group_by_having.sql
@@ -203,3 +203,16 @@ FROM vendor_inventory
 WHERE market_date BETWEEN '2019-04-03' AND '2019-04-16'
 GROUP BY vendor_id
 ORDER BY vendor_id;
+
+-- ------------------------- average -----------------------------
+-- Obtener el precio promedio de los productos de cada proveedor
+SELECT
+	vendor_id,
+    COUNT(DISTINCT product_id) AS different_products_offered,
+    SUM(quantity * original_price) AS value_of_inventory,
+    SUM(quantity) AS inventory_item_count,
+    ROUND(SUM(quantity * original_price)/SUM(quantity), 2) AS average_item_price
+FROM vendor_inventory
+WHERE market_date BETWEEN '2019-04-03' AND '2019-04-16'
+GROUP BY vendor_id
+ORDER BY vendor_id;
