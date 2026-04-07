90bebbc78506211176c9234de4540e8276c19ada
jbernalg <jeinffersonbernal@gmail.com>
2026-04-07 08:46:07 -0500

    LAG 1.2

--- SQL_for_Data_Scientist/window_functionn_subqueries.sql
+++ SQL_for_Data_Scientist/window_functionn_subqueries.sql
@@ -162,3 +162,19 @@ SELECT
 FROM vendor_booth_assignments
 ORDER BY market_date, vendor_id, booth_number;
 
+-- filtrar resultado anterior a una fecha especifica para determinar que proveedores son nuevos
+-- o estan cambiando de puesto ese dia 
+SELECT * FROM
+(
+	SELECT
+		market_date,
+		vendor_id,
+		booth_number,
+		LAG(booth_number, 1) OVER (PARTITION BY vendor_id ORDER BY market_date, vendor_id) AS previous_booth_number
+	FROM vendor_booth_assignments
+	ORDER BY market_date, vendor_id, booth_number
+) AS x
+WHERE x.market_date = '2019-04-10'
+	AND (x.booth_number <> x.previous_booth_number OR x.previous_booth_number IS NULL);
+
+
