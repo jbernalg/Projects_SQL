a44913e70913498f2e1af18f7f1101992c64a3b5
jbernalg <jeinffersonbernal@gmail.com>
2026-04-09 08:19:16 -0500

    EXTRACT

--- SQL_for_Data_Scientist/date_time_functions.sql
+++ SQL_for_Data_Scientist/date_time_functions.sql
@@ -16,3 +16,15 @@ CREATE TABLE farmers_market.datetime_demo AS (
 );
 
 SELECT * FROM datetime_demo;
+
+-- ------------------------------- EXTRACT y DATE_PART --------------------------
+-- obtener todas las partes de un datetime de una fecha especifica
+SELECT
+	market_start_datetime,
+	EXTRACT(DAY FROM market_start_datetime) AS msd_day,
+    EXTRACT(MONTH FROM market_start_datetime) AS msd_month,
+    EXTRACT(YEAR FROM market_start_datetime) AS msd_year,
+    EXTRACT(HOUR FROM market_start_datetime) AS msd_hour,
+    EXTRACT(MINUTE FROM market_start_datetime) AS msd_minute
+FROM datetime_demo
+WHERE market_start_datetime = '2019-03-02 08:00:00';
